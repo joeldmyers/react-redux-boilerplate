38f6c9dca8c63005f3433d19ba167fa584fcd653
Joel <joeldmyers@gmail.com>
2020-05-02 14:22:20 -0400

    eslintrc update, using useState hook to get counter working, sans redux

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>Counter!</h1>
-      <div id="app-counter">0</div>
-      <button type="button">-</button>
-      <button type="button">-</button>
+      <div id="app-counter">{count}</div>
+      <button type="button" onClick={() => setCount(count - 1)}>
+        -
+      </button>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        -
+      </button>
     </div>
   );
 };
