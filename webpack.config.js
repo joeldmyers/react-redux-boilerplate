52afd34588cf1f58d4103e9cb869214e45824abc
Joel <joeldmyers@gmail.com>
2020-05-02 18:17:50 -0400

    getting embedded scss to work

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,24 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "sass-loader"],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
     ],
   },
